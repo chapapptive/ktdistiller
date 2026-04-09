b6f014b379113241032c220dab73e3d38554b32c
chapapptive <92938266+chapapptive@users.noreply.github.com>
2026-04-08 21:28:23 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Replace 'kt-distiller' with your actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/kt-distiller/',
+  base: '/ktdistiller/',
 })
